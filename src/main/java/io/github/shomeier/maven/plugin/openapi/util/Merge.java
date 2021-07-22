5ec67b3b14541dda1dd0fb3191653f76cda7f81b
Sascha Homeier <sascha.homeier@gmail.com>
2021-07-22 21:43:33 +0700

    #2 Using maven resources model

--- src/main/java/io/github/shomeier/maven/plugin/openapi/util/Merge.java
+++ src/main/java/io/github/shomeier/maven/plugin/openapi/util/Merge.java
@@ -3,27 +3,39 @@ package io.github.shomeier.maven.plugin.openapi.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.Scanner;
+import org.sonatype.plexus.build.incremental.BuildContext;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 
 @Mojo(name = Merge.GOAL, defaultPhase = LifecyclePhase.INITIALIZE)
 public class Merge extends AbstractMojo {
     public static final String GOAL = "merge";
+
+    private static final String[] EMPTY_STRING_ARRAY = {};
+
+    private static final String[] DEFAULT_INCLUDES = {"**/**"};
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
@@ -31,28 +43,89 @@ public class Merge extends AbstractMojo {
     @Parameter(required = true)
     private File headerFile;
 
-    @Parameter(defaultValue = "${project.basedir}/src/main/resources", required = true)
-    private File inputDirectory;
+    @Parameter(defaultValue = "${project.resources}", required = true, readonly = true)
+    private List<Resource> resources;
 
     @Parameter(required = true)
     private File outputFile;
+
+    @Parameter(defaultValue = "${session}", readonly = true, required = true)
+    protected MavenSession session;
+
+    @Requirement
+    private BuildContext buildContext;
 
     public void execute() throws MojoExecutionException {
         validateParameters();
 
         try {
             FileUtils.copyFile(headerFile, outputFile);
-            try (Stream<Path> files = Files.walk(inputDirectory.toPath())) {
-                Map<Path, OpenAPI> filePaths = files
-                        .filter(p -> p.toFile().isFile() &&
-                                (p.toString().endsWith(".yaml") || p.toString().endsWith(".yml")))
-                        .collect(Collectors.toMap(p -> p, this::parse));
 
-                merge(outputFile.toPath(), filePaths);
-            }
+            List<Path> includedFiles = getIncludedFiles();
+            Map<Path, OpenAPI> filePaths = includedFiles.stream()
+                    .collect(Collectors.toMap(p -> p, this::parse));
+
+            merge(outputFile.toPath(), filePaths);
         } catch (IOException e) {
             throw new MojoExecutionException("Error while merging", e);
         }
+    }
+
+    List<Path> getIncludedFiles() {
+
+        List<Path> includedFiles = new ArrayList<>();
+
+        for (Resource resource : getResources()) {
+
+            File resourceDirectory = new File(resource.getDirectory());
+
+            if (!resourceDirectory.isAbsolute()) {
+                // TODO;
+                // resourceDirectory =
+                // new File( mavenResourcesExecution.getResourcesBaseDirectory(), resourceDirectory.getPath() );
+            }
+
+            if (!resourceDirectory.exists()) {
+                getLog().info("skip non existing resourceDirectory " + resourceDirectory.getPath());
+                continue;
+            }
+
+            // Scanner scanner = buildContext.newScanner(resourceDirectory, true);
+            DirectoryScanner scanner = new DirectoryScanner();
+            scanner.setBasedir(resourceDirectory);
+
+            setupScanner(resource, scanner, true);
+
+            scanner.scan();
+            List<Path> paths = Arrays.asList(scanner.getIncludedFiles()).stream()
+                    .map(s -> java.nio.file.Paths.get(resourceDirectory.getAbsolutePath(), s))
+                    .collect(Collectors.toList());
+
+            includedFiles.addAll(paths);
+        }
+
+        return includedFiles;
+    }
+
+    private String[] setupScanner(Resource resource, Scanner scanner, boolean addDefaultExcludes) {
+        String[] includes = null;
+        if (resource.getIncludes() != null && !resource.getIncludes().isEmpty()) {
+            includes = resource.getIncludes().toArray(EMPTY_STRING_ARRAY);
+        } else {
+            includes = DEFAULT_INCLUDES;
+        }
+        scanner.setIncludes(includes);
+
+        String[] excludes = null;
+        if (resource.getExcludes() != null && !resource.getExcludes().isEmpty()) {
+            excludes = resource.getExcludes().toArray(EMPTY_STRING_ARRAY);
+            scanner.setExcludes(excludes);
+        }
+
+        if (addDefaultExcludes) {
+            scanner.addDefaultExcludes();
+        }
+        return includes;
     }
 
     private OpenAPI parse(Path path) {
@@ -70,13 +143,16 @@ public class Merge extends AbstractMojo {
 
         for (Entry<Path, OpenAPI> filePathEntry : filePaths.entrySet()) {
 
-            for (Entry<String, PathItem> sourcePaths : filePathEntry.getValue().getPaths().entrySet()) {
+            Paths paths = filePathEntry.getValue().getPaths();
+            if (paths != null) {
+                for (Entry<String, PathItem> sourcePaths : paths.entrySet()) {
 
-                // relativize assumes the path is a directory that is why we need to relativize from parnet
-                Path relativePath = targetFile.getParent().relativize(filePathEntry.getKey());
-                String ref = buildRef(relativePath, sourcePaths.getKey());
-                PathItem pathItem = new PathItem().$ref(ref);
-                target.path(sourcePaths.getKey(), pathItem);
+                    // relativize assumes the path is a directory that is why we need to relativize from parnet
+                    Path relativePath = targetFile.getParent().relativize(filePathEntry.getKey());
+                    String ref = buildRef(relativePath, sourcePaths.getKey());
+                    PathItem pathItem = new PathItem().$ref(ref);
+                    target.path(sourcePaths.getKey(), pathItem);
+                }
             }
         }
         String pathsString = Yaml.pretty().writeValueAsString(target);
@@ -89,13 +165,6 @@ public class Merge extends AbstractMojo {
 
     private void validateParameters() throws MojoExecutionException {
         validateFile(headerFile, "headerFile");
-
-        validateDirectory(inputDirectory, "inputDirectory");
-        if (inputDirectory.listFiles().length == 0) {
-            throw new MojoExecutionException(
-                    String.format("The specified inputDirectory '%s' is empty!", inputDirectory));
-        }
-
         validateParam(outputFile, "outputFile");
     }
 
@@ -107,20 +176,6 @@ public class Merge extends AbstractMojo {
         }
     }
 
-    private void validateDirectory(File directory, String paramName)
-            throws MojoExecutionException {
-
-        validateParam(directory, paramName);
-        if (!directory.exists()) {
-            throw new MojoExecutionException(
-                    String.format("The specified %s '%s' does not exist!", paramName, directory));
-        }
-        if (!directory.isDirectory()) {
-            throw new MojoExecutionException(
-                    String.format("The specified %s '%s' is not a directory!", paramName, directory));
-        }
-    }
-
     private void validateParam(Object param, String paramName) throws MojoExecutionException {
         if (param == null) {
             throw new MojoExecutionException(
@@ -128,4 +183,17 @@ public class Merge extends AbstractMojo {
         }
     }
 
+    /**
+     * @return {@link #resources}
+     */
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    /**
+     * @param resources set {@link #resources}
+     */
+    public void setResources(List<Resource> resources) {
+        this.resources = resources;
+    }
 }
