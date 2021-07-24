d0ac6a10db6025f7a2eb632a940ec7f6d4a0a4d9
Sascha Homeier <sascha.homeier@gmail.com>
2021-07-24 17:34:58 +0700

    #2 Refactored out openapi merging methods

--- src/main/java/io/github/shomeier/maven/plugin/openapi/util/Merge.java
+++ src/main/java/io/github/shomeier/maven/plugin/openapi/util/Merge.java
@@ -2,15 +2,8 @@ package io.github.shomeier.maven.plugin.openapi.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Resource;
@@ -20,14 +13,6 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.component.annotations.Requirement;
-import org.sonatype.plexus.build.incremental.BuildContext;
-import io.swagger.v3.core.util.Yaml;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.PathItem;
-import io.swagger.v3.oas.models.Paths;
-import io.swagger.v3.parser.ObjectMapperFactory;
-import io.swagger.v3.parser.OpenAPIV3Parser;
 
 @Mojo(name = Merge.GOAL, defaultPhase = LifecyclePhase.INITIALIZE)
 public class Merge extends AbstractMojo {
@@ -51,9 +36,6 @@ public class Merge extends AbstractMojo {
     @Parameter(defaultValue = "${session}", readonly = true, required = true)
     protected MavenSession session;
 
-    @Requirement
-    private BuildContext buildContext;
-
     public void execute() throws MojoExecutionException {
         validateParameters();
 
@@ -62,67 +44,13 @@ public class Merge extends AbstractMojo {
 
             ResourcesResolver resolver = new ResourcesResolver(resources, project, getLog());
             List<Path> includedFiles = resolver.getIncludedFiles();
-            Map<Path, OpenAPI> filePaths = includedFiles.stream()
-                    .collect(Collectors.toMap(p -> p, this::parse));
 
-            merge(outputFile.toPath(), filePaths);
+            Merger merger = new Merger(outputFile, expandPaths);
+            merger.merge(includedFiles);
+
         } catch (IOException e) {
             throw new MojoExecutionException("Error while merging", e);
         }
-    }
-
-    private OpenAPI parse(Path path) {
-        return new OpenAPIV3Parser().read(path.toString());
-    }
-
-    private void merge(Path targetFile, Map<Path, OpenAPI> filePaths) throws IOException {
-
-        OpenAPI target = new OpenAPIV3Parser().read(targetFile.toString());
-
-        // JsonFactory factory = Yaml.mapper().getFactory();
-        // factory.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
-        // .enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
-        // .enable(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS);
-
-        for (Entry<Path, OpenAPI> filePathEntry : filePaths.entrySet()) {
-
-            Paths paths = filePathEntry.getValue().getPaths();
-            if (paths != null) {
-                for (Entry<String, PathItem> sourcePaths : paths.entrySet()) {
-
-                    // relativize assumes the path is a directory that is why we need to relativize from parnet
-                    Path relativePath = targetFile.getParent().relativize(filePathEntry.getKey());
-                    String ref = buildRef(relativePath, sourcePaths.getKey());
-                    PathItem pathItem = sourcePaths.getValue();
-                    if (!expandPaths) {
-                        pathItem = new PathItem().$ref(ref);
-                    }
-                    target.path(sourcePaths.getKey(), pathItem);
-                }
-            }
-        }
-
-        String pathsString = Yaml.pretty().writeValueAsString(target);
-        FileUtils.writeStringToFile(targetFile.toFile(), pathsString, Charset.defaultCharset(), false);
-
-        // Another option to write yamls here
-        // ObjectMapper yamlMapper = createYaml();
-        // String pathsString = yamlMapper.writer(new DefaultPrettyPrinter()).writeValueAsString(target);
-        // FileUtils.writeStringToFile(targetFile.toFile(), pathsString, StandardCharsets.UTF_8, false);
-    }
-
-    protected static ObjectMapper createYaml() {
-        ObjectMapper yamlMapper = ObjectMapperFactory.createYaml();
-        ((YAMLFactory) yamlMapper.getFactory()).disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
-        ((YAMLFactory) yamlMapper.getFactory()).enable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
-        ((YAMLFactory) yamlMapper.getFactory()).enable(YAMLGenerator.Feature.SPLIT_LINES);
-        ((YAMLFactory) yamlMapper.getFactory()).enable(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS);
-        ((YAMLFactory) yamlMapper.getFactory()).enable(YAMLGenerator.Feature.INDENT_ARRAYS);
-        return yamlMapper;
-    }
-
-    private String buildRef(Path relativePath, String oaPath) {
-        return "./" + relativePath.toString() + "#/paths/" + oaPath.replace("/", "~1");
     }
 
     private void validateParameters() throws MojoExecutionException {
