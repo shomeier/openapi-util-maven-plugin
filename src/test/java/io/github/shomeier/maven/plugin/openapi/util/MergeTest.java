40576da8f4aaff4154792fed8e434a6ca64c1109
Sascha Homeier <sascha.homeier@gmail.com>
2021-08-15 21:30:31 +0700

    #10 Fixed test

--- src/test/java/io/github/shomeier/maven/plugin/openapi/util/MergeTest.java
+++ src/test/java/io/github/shomeier/maven/plugin/openapi/util/MergeTest.java
@@ -9,8 +9,12 @@ import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.WithoutMojo;
 import org.junit.Rule;
 import org.junit.Test;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.ParseOptions;
 
 public class MergeTest {
+
     @Rule
     public MojoRule rule = new MojoRule() {
         @Override
@@ -20,9 +24,6 @@ public class MergeTest {
         protected void after() {}
     };
 
-    /**
-     * @throws Exception if any
-     */
     @Test
     public void testMergeSimple() throws Exception {
         File pom = new File("target/test-classes/merge-simple");
@@ -45,9 +46,6 @@ public class MergeTest {
                 FileUtils.readFileToString(outputFile, "utf-8"));
     }
 
-    /**
-     * @throws Exception if any
-     */
     @Test
     public void testMergeIncludes() throws Exception {
         File pom = new File("target/test-classes/merge-includes");
@@ -95,25 +93,28 @@ public class MergeTest {
     @Test
     public void testMergeResolve() throws Exception {
 
-            String prjName = "merge-resolve";
-            File pom = new File("target/test-classes/" + prjName);
-            assertNotNull(pom);
-            assertTrue(pom.exists());
+        String prjName = "merge-resolve";
+        File pom = new File("target/test-classes/" + prjName);
+        assertNotNull(pom);
+        assertTrue(pom.exists());
 
-            Merge myMojo = (Merge) rule.lookupConfiguredMojo(pom, "merge");
-            assertNotNull(myMojo);
-            myMojo.execute();
+        Merge myMojo = (Merge) rule.lookupConfiguredMojo(pom, "merge");
+        assertNotNull(myMojo);
+        myMojo.execute();
 
-            File outputFile = (File) rule.getVariableValueFromObject(myMojo, "outputFile");
-            assertNotNull(outputFile);
-            assertTrue(outputFile.exists());
+        File outputFile = (File) rule.getVariableValueFromObject(myMojo, "outputFile");
+        assertNotNull(outputFile);
+        assertTrue(outputFile.exists());
 
-            ClassLoader classLoader = getClass().getClassLoader();
-            File expectedFile = new File(classLoader
-                            .getResource(prjName + "/src/main/resources/expectedOutput.yaml").getFile());
-            assertEquals("The files differ!",
-                            FileUtils.readFileToString(expectedFile, "utf-8"),
-                            FileUtils.readFileToString(outputFile, "utf-8"));
+        // the order of the schema components is not guaranteed 
+        ParseOptions options = new ParseOptions();
+        options.setResolve(true);
+        OpenAPI openApi = new OpenAPIV3Parser().read(outputFile.getAbsolutePath(), null, options);
+        assertTrue(openApi.getComponents().getSchemas().containsKey("Store"));
+        assertTrue(openApi.getComponents().getSchemas().containsKey("Stores"));
+        assertTrue(openApi.getComponents().getSchemas().containsKey("Pet"));
+        assertTrue(openApi.getComponents().getSchemas().containsKey("Pets"));
+        assertTrue(openApi.getComponents().getSchemas().containsKey("Error"));
     }
 
     @Test
